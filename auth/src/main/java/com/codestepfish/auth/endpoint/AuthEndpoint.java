2de72d6b8ad0ff03910ca7969af4a310f8913edf
bootvue <bootvue@gmail.com>
2022-08-24 22:28:42 +0800

    fix auth uri

--- auth/src/main/java/com/codestepfish/auth/endpoint/AuthEndpoint.java
+++ auth/src/main/java/com/codestepfish/auth/endpoint/AuthEndpoint.java
@@ -9,16 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
+@RequestMapping("/oauth")
 public class AuthEndpoint {
 
     private final AuthService authService;
